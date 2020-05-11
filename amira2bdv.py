ec2d9d7f6ea2ed6706fe283fbdb2e8fc75fb7520
Martin Schorb <schorb@embl.de>
2020-05-11 18:02:39 +0200

    amirabdv bi-directional

--- amira2bdv.py
+++ amira2bdv.py
@@ -10,13 +10,22 @@ import mrcfile as mrc
 import os
 from skimage import io
 
+import xml.etree.ElementTree as ET
+
+
+
 bdv_unit = 'um'
 
+ov_mode = 'metadata'
 
 # if individual BDV data files should be created for every Amira data object. I false, all will end up in one file.
 split_files = True 
 
 mrcl = ['mrc','st','rec','join','map']
+
+ski_types = ['tif','png']
+
+ov_data = ['data','all']
 
 
 timept = 0
@@ -79,9 +88,8 @@ for tf_file in os.listdir():
         latt = np.array(list(map(float,tfile[4].split(',')[0].split(' x '))))
         
         #import file list        
-        files = tfile[5].split(' ')
-        
-        
+        files = tfile[5].split(' , ')
+
         
 #        ------------------------------------------------
         
@@ -95,32 +103,7 @@ for tf_file in os.listdir():
         bbox_n[1]=bbox_n[1]-bbox_0[0];bbox_n[3]=bbox_n[3]-bbox_0[2];bbox_n[5]=bbox_n[5]-bbox_0[4]
         
                
-        
-        if len(files)<2:
-            im_file = files[0]
-            # single file for the volume or 2D data
-            if 'tif' in im_file[im_file.rindex(os.path.extsep):]:
-                data = io.imread(im_file)
-            elif any(ext in im_file for ext in mrcl):
-                data = mrc.mmap(im_file).data
-                
-                
-                
-        else:
-            # list of slices as a stack
-            data = [];
-            
-            for im_file in files:            
-                if 'tif' in im_file[im_file.rindex(os.path.extsep):]:
-                    data0 = io.imread(im_file)
-                    if len(data)==0:
-                        data = np.expand_dims(data0.copy(),axis=0)
-                    else:
-                        data = np.concatenate((data,np.expand_dims(data0.copy(),axis=0)),axis=0)
-            
-
-            
-            
+         
         
         # compensate with initial translation (when opening Amira)
         
@@ -165,33 +148,139 @@ for tf_file in os.listdir():
         view['trafo']['Amira_Translation'] = tf_tr
         view['trafo']['Amira_Transform'] = tf_tform     
         view['trafo']['Amira_Origin_Translation'] = tf_or
-     
-             
-
+    
        
+        
         
         outfile = os.path.join('bdv',outname+outformat) 
         
+        
+        data = []
             
-    
-        ndim = data.ndim
-        if ndim > 2: assert ndim == 3, "Only support 3d"
-            #assert len(resolution) == ndim
-        if ndim < 3: 
-            assert ndim == 2, "Only support 2d"
-            data=np.expand_dims(data.copy(),axis=0)
+        if len(files)<2:
+            im_file = files[0]
+            # single file for the volume or 2D data
+            if '.xml.bin' in tf_file:
+                # downsampled data from original BDV source
+                binning = int(tf_file[tf_file.find('.xml.bin') + 8])
+                tform[0:3,0:3] = 1 / binning * tform[0:3,0:3] 
+                
+                tform1 =  tform.T * np.append(voxs,1)
+                tf_tform = tf.matrix_to_transformation(tform1).tolist()
+                
+                view['trafo']['Amira_Transform'] = tf_tform
+                
+                trans =  1 / binning * trans
+                
+                mat_t = np.concatenate((np.eye(3),np.array([[trans[0]],[trans[1]],[trans[2]]])),axis=1)
+                mat_t = np.concatenate((mat_t,[[0,0,0,1]]))
         
-        
-        pybdv.make_bdv(data1,outfile,downscale_factors,
-                   resolution = view['resolution'],
-                   unit = bdv_unit, 
-                   setup_id = view['setup_id'],
-                   timepoint = timept,
-                   setup_name = view['setup_name'],
-                   attributes = view['attributes'],
-                   affine = view['trafo'],
-                   overwrite = 'all')
-        
+                tf_tr = tf.matrix_to_transformation(mat_t).tolist()
+                
+                view['trafo']['Amira_Translation'] = tf_tr
+                
+                xml_orig = im_file[0:im_file.find('.xml.bin') + 4]
+                
+                
+                
+                if os.path.exists(xml_orig):
+                    # check original bdv xml file for data location
+                    ov_mode = 'metadata'
+                    tree = ET.parse(xml_orig)
+                    root = tree.getroot() 
+                    # load the file location
+                    base_path = root.find('BasePath').text
+                    if base_path=='.': base_path=''
+                   
+                    seqdesc = root.find('SequenceDescription')
+                   
+                    imload = seqdesc.find('ImageLoader')                        
+                    im_format = imload.attrib['format']
+                   
+                    im_desc = imload[0]
+                    
+                    data_path = '/'.join([os.path.dirname(im_file),base_path, im_desc.text])
+                    
+                    is_h5=False
+                    
+                    if outformat=='.h5': is_h5=True
+                    
+                    
+                    if os.access(xml_orig,os.W_OK):
+                        # if data locatin is writable, create new file here
+                        outname = im_file[0:im_file.find('.xml.bin')]+'_amira'                       
+                        
+                        
+                    else:
+                        # create new xml file in current lints to the original data
+                        cwd = os.getcwd()
+
+                        com_path = os.path.commonpath([cwd,xml_orig])
+                        
+                        rel_path = os.path.relpath(os.path.dirname(xml_orig),cwd)
+                        xml_dir = '/'.join(rel_path.split(os.path.sep))
+                        
+                        if xml_dir[0]=='/': xml_dir=xml_dir[1:]
+                        
+                        data_path = '/'.join([xml_dir,base_path, im_desc.text])
+                        
+                                                
+                        
+                    pybdv.metadata.write_xml_metadata(outname + '.xml', data_path,
+                                                          resolution = view['resolution'],
+                                                          unit = bdv_unit,setup_id = view['setup_id'],
+                                                          timepoint = timept,setup_name = view['setup_name'],
+                                                          attributes = view['attributes'],
+                                                          affine = view['trafo'],
+                                                          overwrite = True,
+                                                          is_h5=is_h5,
+                                                          overwrite_data=False,
+                                                          enforce_consistency=True)
+ 
+               # outname = tf_file[0:tf_file.find('.xml.bin')]
+               # data = [];
+                
+            elif any(ext in os.path.basename(im_file) for ext in ski_types):
+                data = io.imread(im_file)
+            elif any(ext in os.path.basename(im_file) for ext in mrcl):
+                data = mrc.mmap(im_file).data
+                
+                
+                
+        else:
+            # list of slices as a stack
+            if ((ov_mode in ov_data) | os.path.exists(outfile)==False):                
+                # check if bdv data already exists
+                for im_file in files:            
+                    if any(ext in os.path.basename(im_file) for ext in ski_types):
+                        print('Reading image data from: ' + im_file)
+                        data0 = io.imread(im_file)
+                        if len(data)==0:
+                            data = np.expand_dims(data0.copy(),axis=0)
+                        else:
+                            data = np.concatenate((data,np.expand_dims(data0.copy(),axis=0)),axis=0)                
+            else:
+                data = np.zeros([1,1,1])
+
+        if len(data)>0:
+            ndim = data.ndim
+            if ndim > 2: assert ndim == 3, "Only support 3d"
+                #assert len(resolution) == ndim
+            if ndim < 3: 
+                assert ndim == 2, "Only support 2d"
+                data=np.expand_dims(data.copy(),axis=0)
+            
+            print('Writing BDV output data for '+outfile)
+            pybdv.make_bdv(data,outfile,downscale_factors,
+                       resolution = view['resolution'],
+                       unit = bdv_unit, 
+                       setup_id = view['setup_id'],
+                       timepoint = timept,
+                       setup_name = view['setup_name'],
+                       attributes = view['attributes'],
+                       affine = view['trafo'],
+                       overwrite = ov_mode)
+            
         
         
                 
