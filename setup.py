580e80eb2a96f086872377ef378d0362277680d9
Martin Schorb <schorb@embl.de>
2020-06-30 17:51:05 +0200

    update version to 20200630

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='bdv_tools',
-      version='20200626',
+      version='20200630',
       py_modules=['bdv_tools'],
       description='Tools to interact with BigDataViewer.',
       long_description=long_description,
