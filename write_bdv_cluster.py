d2d26d21ca7d49894d7ed45a797d40ee359a153f
Martin Schorb <schorb@embl.de>
2020-07-09 15:40:47 +0200

    cluster script ready

--- write_bdv_cluster.py
+++ write_bdv_cluster.py
@@ -4,6 +4,13 @@
 
 import bdv_tools as bdv
 import sys
+import os
+
+
+
+
+mrcext = ['.mrc','.rec','.st']
+tifext = ['.tif','.tiff']
 
 
 infile = sys.argv[1]
@@ -19,5 +26,37 @@ if len(downscale_factors)<3:downscale_factors = None
 print('starting conversion to bdv on the cluster for '+infile)
 
 view = bdv.xml2dict(view_xml)
+
+
+# read data
+
+if os.path.splitext(infile)[-1].lower() in mrcext:
+    
+    import mrcfile as mrc
+    import numpy as np
+    
+    mfile = mrc.mmap(infile,permissive = 'True')
+    data0 = mfile.data
+
+
+    # check if volume is rotated
+    if data0.shape[0]/data0.shape[1]>5:
+        data0 = np.swapaxes(data0,0,1)
+
+    data0 = np.swapaxes(data0,0,2)
+    data1 = np.fliplr(data0)
+    data = np.swapaxes(data1,0,2)
+    
+elif os.path.splitext(infile)[-1].lower() in tifext:
+    
+    from skimage import io
+    
+    data  = io.imread(infile)
+    
+
+
+bdv.write_bdv(outfile,data,view,blow_2d=1,downscale_factors=downscale_factors,cluster=False,infile=infile)
+
+    
 
 
