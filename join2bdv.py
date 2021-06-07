f8c7ff5a7746fad8440b2c22e4e9b83d77533309
Martin Schorb <martin.schorb@embl.de>
2021-06-07 09:20:00 +0200

    join4Sebastian

---
+++ join2bdv.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  7 08:27:24 2021
+
+@author: schorb
+"""
+
+import numpy as np
+import pyEM as em
+
+#import pybdv
+from pybdv import transformations as tf
+import mrcfile as mrc
+import glob
+import re
+import sys
+import os
+
+from skimage import io
+
+import bdv_tools as bdv
+
+
+cluster = True
+
+
+bdv_unit = 'um'
+zstretch = 1
+timept = 0
+
+outformat='.n5'
+
+chunks = list((16,192,192))
+downscale_factors = list(([1,2,2],[1,2,2],[1,2,2],[1,2,2],[1,4,4]))
+
+indir = '/g/schwab/Tobias/Tomography/joined/'
+
+indir = os.path.join(indir,sys.argv[1])
+
+outdir = os.path.join('/g/schwab/Tobias/MoBIE/',sys.argv[1])
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+for file in glob.iglob(indir+'**/*.join', recursive=True):
+    
+    base = os.path.splitext(file.split(indir)[1])[0]
+
+    outfile = os.path.join(outdir,re.sub('/','_',base))+outformat
+
+    # get pixel size
+
+    mfile = mrc.mmap(file,permissive = 'True')
+    tomopx = mfile.voxel_size.x / 10000 # in um
+    
+    mat=np.eye(4)*tomopx
+    mat[2,2] = tomopx*zstretch
+    mat[3,3] = 1
+    
+    tf_tr = tf.matrix_to_transformation(mat).tolist()
+
+    setup_id = 0
+
+    view=dict()
+
+    view['resolution'] = [tomopx,tomopx,tomopx*zstretch]
+    view['setup_id'] = setup_id
+    view['setup_name'] = os.path.basename(base)
+
+    view['OriginalFile'] = file
+
+    view['trafo'] = dict()
+    view['trafo']['Scaling'] = tf_tr
+
+    view['attributes'] = dict()
+
+    view['attributes']['displaysettings'] = dict({'id':setup_id,'color':bdv.colors['W'],'isset':'true'})
+    view['attributes']['displaysettings']['Projection_Mode'] = 'Average'
+
+    view['attributes']['displaysettings']['min']=-127
+    view['attributes']['displaysettings']['max']=127
+
+    data = mfile.data
+
+
+    # check if volume is rotated
+    if data.shape[0]/data.shape[1]>5:
+        data = np.swapaxes(data,0,1)
+
+    data0 = np.swapaxes(data,0,2)
+    data1 = np.fliplr(data0)
+    data2 = np.swapaxes(data1,0,2)
+
+    print(outfile)
+
+    bdv.write_bdv(outfile,data2,view,outf='.n5',blow_2d=zstretch,downscale_factors=downscale_factors,cluster=cluster,infile=file,chunks=chunks)
